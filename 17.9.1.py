b471563b8184790d773353d60242101768f18b3e
SergesQ <116515983+SergesQ@users.noreply.github.com>
2023-05-09 19:06:44 +0300

    Add files via upload

---
+++ 17.9.1.py
@@ -0,0 +1,37 @@
+
+array = input("Введите последовательность целых чисел через пробел:\n")
+
+while array.replace(" ", "").isdigit() != True:
+    print("Введены недопустимые символы.\nИспользуйте только целые числа и пробел")
+    array = input("Введите последовательность целых чисел через пробел:\n")
+
+array = list(map(int, array.split(' ')))
+for i in range(len(array)):
+    idx_min = i
+    for j in range(i, len(array)):
+        if array[j] < array[idx_min]:
+            idx_min = j
+    if i != idx_min:
+        array[i], array[idx_min] = array[idx_min], array[i]
+
+def binary_search(array, element, left, right):
+    if left > right:
+        return False
+    middle = (right + left) // 2
+    if array[middle] == element:
+        return f"Ваше число имеет индекс {middle}"
+    elif element < array[middle]:
+        if left == right or left == middle:
+            return f"Ваше число имеет индекс {middle}"
+        return binary_search(array, element, left, middle - 1)
+    elif element >= array[middle]:
+        if left == right:
+            return f"Ваше число имеет индекс {middle + 1}"
+        return binary_search(array, element, middle + 1, right)
+
+print(f'Ваш список, отсортированный по возрастанию: {array}')
+element = input('Введите целое число: ')
+while element.isdigit() != True:
+    element = input("Ошибка ввода. Введите целое число: ")
+element = int(element)
+print(binary_search(array, element, 0, len(array ) -1))
