192e0fb1665c11600b92f26253bd724387322b8a
SergesQ <116515983+SergesQ@users.noreply.github.com>
2023-05-16 22:12:04 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5804222789:AAH6AJuYmUEnSNGlUSmyyFmdw4PO7p8uUD8'
+
+keys = {
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
